f79b13905522c55c0dcd3ad5605f42af7af09d87
Ankur Singh <ankur13019@iiitd.ac.in>
2015-11-23 09:25:49 +0530

    image upload process complete

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -5,14 +5,35 @@ $(document).on('ready',function(){
 	$('#edit').on('click',function(){
 		launchEditor('preview');
 	});
+	$("input[id='image_image").on('change',function(e){
+		for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
+
+      var file = e.originalEvent.srcElement.files[i];
+
+      var img = $('#preview');
+      var reader = new FileReader();
+      reader.onloadend = function() {
+           img.attr('src',reader.result);
+      }
+      reader.readAsDataURL(file);
+    }
+	});
+
+	$("input[id='image_temp']").on('change',function(e){
+     var img = $('#preview');
+     var val = $("input[id='image_temp']").val();
+     img.attr('src',val);
+     $("input[id='image_remote_image_url']").val(val);
+	});
 	var featherEditor = new Aviary.Feather({
-		apiKey: 'your-client-id-here',
+		apiKey: 'b2da2d33ca1f4e9e8a1d825932760d12',
 		theme: 'dark', // Check out our new 'light' and 'dark' themes!
 		tools: 'all',
 		appendTo: '',
 		onSave: function(imageID, newURL) {
 			var img = document.getElementById(imageID);
 			img.src = newURL;
+      $("input[id='image_remote_image_url']").val(newURL);
 		},
 		onError: function(errorObj) {
 			alert(errorObj.message);
